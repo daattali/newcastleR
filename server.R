db69d48275530b442bea83358ff884c60db0747c
Dean Attali <daattali@gmail.com>
2021-01-25 16:58:34 -0500

    use multi file instead of app.R

--- server.R
+++ server.R
@@ -1,61 +1,4 @@
-library(shiny)
-library(ggplot2)
-library(magrittr)
-
-ui <- fluidPage(
-
-  shinyjs::useShinyjs(),
-
-  titlePanel("EPL 2019-2020 (data from fbref.com)"),
-
-  tabsetPanel(
-
-    tabPanel(
-      "Explore players",
-      fluidRow(
-        column(
-          4,
-          numericInput("num_matches", "Minimum matches", 5)
-        ),
-        column(4, selectInput("position",
-                              "Position",
-                              c("All", "Goalkeepers", "Defenders", "Midfielders", "Forwards")))
-      ),
-      DT::DTOutput("main_data"),
-      actionButton("predict", "Predict player goals next season"),
-      textOutput("prediction")
-    ),
-
-    tabPanel(
-      "Plot",
-      fluidRow(
-        column(
-          4,
-          uiOutput("teams_selector_ui")
-        ),
-        column(
-          4,
-          selectInput("xvar", "X axis", c("Goals", "Assists", "Minutes", "Age"), selected = "Goals")
-        ),
-        column(
-          4,
-          selectInput("yvar", "X axis", c("Goals", "Assists", "Minutes", "Age"), selected = "Minutes")
-        )
-      ),
-      actionLink("plot_show_options", "Show plot options"),
-      fluidRow(
-        id = "plot_options",
-        column(4, sliderInput("plot_marker_size", "Marker size", 1, 5, 1)),
-        column(4, colourpicker::colourInput("plot_marker_col", "Marker colour", "#471D85")),
-        column(4, colourpicker::colourInput("plot_col", "Plot colour", "#EDEEFF"))
-      ) %>% shinyjs::hidden(), br(),
-      actionButton("do_plot", "Plot!"),
-      plotOutput("plot")
-    )
-  )
-)
-
-server <- function(input, output, session) {
+function(input, output, session) {
 
   players_data <- read.csv("data/playersdata.csv",encoding = "UTF-8")
 
@@ -138,25 +81,6 @@ server <- function(input, output, session) {
     })
   })
 
-  prediction <- eventReactive(input$predict, {
-    player <- filtered_data()[input$main_data_rows_selected, ]
-    goals <- player$goals
-    if (grepl("FW", player$position)) {
-      if (player$age <= 25) {
-        goals <- goals + 4
-      } else if (player$age <= 32) {
-        goals <- goals + 1
-      } else {
-        goals <- goals - 2
-      }
-    } else if (player$position == "GK") {
-      stop("Our prediction algorithm breaks for goalkeepers")
-    } else {
-      goals <- max(0, goals + sample(-2:2, 1))
-    }
-    goals
-  })
-
   output$teams_selector_ui <- renderUI({
     selectInput("plot_team", "Team", choices = c("All" = "", unique(players_data$team)), multiple = TRUE)
   })
@@ -178,5 +102,3 @@ server <- function(input, output, session) {
     shinyjs::toggle("plot_options")
   })
 }
-
-shinyApp(ui, server)
---
+++ ui.R
@@ -0,0 +1,53 @@
+fluidPage(
+
+  shinyjs::useShinyjs(),
+
+  titlePanel("EPL 2019-2020 (data from fbref.com)"),
+
+  tabsetPanel(
+
+    tabPanel(
+      "Explore players",
+      fluidRow(
+        column(
+          4,
+          numericInput("num_matches", "Minimum matches", 5)
+        ),
+        column(4, selectInput("position",
+                              "Position",
+                              c("All", "Goalkeepers", "Defenders", "Midfielders", "Forwards")))
+      ),
+      DT::DTOutput("main_data"),
+      br(),
+      actionButton("predict", "Predict player goals next season"),
+      textOutput("prediction", inline = TRUE)
+    ),
+
+    tabPanel(
+      "Plot",
+      fluidRow(
+        column(
+          4,
+          uiOutput("teams_selector_ui")
+        ),
+        column(
+          4,
+          selectInput("xvar", "X axis", c("Goals", "Assists", "Minutes", "Age"), selected = "Goals")
+        ),
+        column(
+          4,
+          selectInput("yvar", "X axis", c("Goals", "Assists", "Minutes", "Age"), selected = "Minutes")
+        )
+      ),
+      actionLink("plot_show_options", "Show plot options"),
+      fluidRow(
+        id = "plot_options",
+        column(4, sliderInput("plot_marker_size", "Marker size", 1, 5, 1)),
+        column(4, colourpicker::colourInput("plot_marker_col", "Marker colour", "#471D85")),
+        column(4, colourpicker::colourInput("plot_col", "Plot colour", "#EDEEFF"))
+      ) %>% shinyjs::hidden(), br(),
+      actionButton("do_plot", "Plot!"),
+      plotOutput("plot")
+    )
+  )
+)
